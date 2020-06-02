707fefd64869890e3a884576ee18b965b27f01b8
Kartik Patel <kartikpatel@fueledbysoda.com>
2020-06-02 18:55:28 -0400

    Updating VisitorDemo to remove unnecessary optionals

--- visitor/src/main/kotlin/com/kotlinbykartik/visitor/VisitorDemo.kt
+++ visitor/src/main/kotlin/com/kotlinbykartik/visitor/VisitorDemo.kt
@@ -1,33 +1,31 @@
 package com.kotlinbykartik.visitor
 
 internal interface CarElement {
-    fun accept(visitor: CarElementVisitor?)
+    fun accept(visitor: CarElementVisitor)
 }
 
 internal interface CarElementVisitor {
-    fun visit(body: Body?)
-    fun visit(car: Car?)
-    fun visit(engine: Engine?)
-    fun visit(wheel: Wheel?)
+    fun visit(body: Body)
+    fun visit(car: Car)
+    fun visit(engine: Engine)
+    fun visit(wheel: Wheel)
 }
 
 internal class Wheel(val name: String) : CarElement {
-
-    override fun accept(visitor: CarElementVisitor?) {
-        visitor!!.visit(this)
+    override fun accept(visitor: CarElementVisitor) {
+        visitor.visit(this)
     }
-
 }
 
 internal class Body : CarElement {
-    override fun accept(visitor: CarElementVisitor?) {
-        visitor!!.visit(this)
+    override fun accept(visitor: CarElementVisitor) {
+        visitor.visit(this)
     }
 }
 
 internal class Engine : CarElement {
-    override fun accept(visitor: CarElementVisitor?) {
-        visitor!!.visit(this)
+    override fun accept(visitor: CarElementVisitor) {
+        visitor.visit(this)
     }
 }
 
@@ -38,46 +36,46 @@ internal class Car : CarElement {
         Body(), Engine()
     )
 
-    override fun accept(visitor: CarElementVisitor?) {
+    override fun accept(visitor: CarElementVisitor) {
         for (element in elements) {
             element.accept(visitor)
         }
-        visitor!!.visit(this)
+        visitor.visit(this)
     }
 }
 
 internal class CarElementDoVisitor : CarElementVisitor {
-    override fun visit(body: Body?) {
+    override fun visit(body: Body) {
         println("Moving my body")
     }
 
-    override fun visit(car: Car?) {
+    override fun visit(car: Car) {
         println("Starting my car")
     }
 
-    override fun visit(wheel: Wheel?) {
-        println("Kicking my " + wheel!!.name + " wheel")
+    override fun visit(wheel: Wheel) {
+        println("Kicking my " + wheel.name + " wheel")
     }
 
-    override fun visit(engine: Engine?) {
+    override fun visit(engine: Engine) {
         println("Starting my engine")
     }
 }
 
 internal class CarElementPrintVisitor : CarElementVisitor {
-    override fun visit(body: Body?) {
+    override fun visit(body: Body) {
         println("Visiting body")
     }
 
-    override fun visit(car: Car?) {
+    override fun visit(car: Car) {
         println("Visiting car")
     }
 
-    override fun visit(engine: Engine?) {
+    override fun visit(engine: Engine) {
         println("Visiting engine")
     }
 
-    override fun visit(wheel: Wheel?) {
-        println("Visiting " + wheel!!.name + " wheel")
+    override fun visit(wheel: Wheel) {
+        println("Visiting " + wheel.name + " wheel")
     }
 }
